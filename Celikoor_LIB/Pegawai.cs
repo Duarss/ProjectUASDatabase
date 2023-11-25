89b563800e8743932df45e832bfc250daf2aec4e
DARIUS MULYADI PUTRA <s160422035@microsoft.ubaya.ac.id>
2023-11-25 17:19:43 +0700

    edit + revisi class pegawai

--- Celikoor_LIB/Pegawai.cs
+++ Celikoor_LIB/Pegawai.cs
@@ -18,6 +18,7 @@ namespace Celikoor_LIB
 
         public Pegawai()
         {
+            Id = 0;
             Nama = "";
             Email = "";
             Username = "";
@@ -41,55 +42,57 @@ namespace Celikoor_LIB
         public string Password { get => password; set => password = value; }
         public string Role { get => role; set => role = value; }
 
-        public static void TambahData(Pegawai p)
+        public static void TambahData(Pegawai pegawai)
         {
-            //susun perintah query
-            string perintah = " INSERT INTO pegawais " + " (id, nama, email, username, password) VALUES " + "('"
-                + p.Id.ToString() + "', '" + p.Nama + "', '" + p.Email + "', '" + p.Username.ToString() + "', '"
-                + p.Password.ToString() + "');";
-            Koneksi.JalankanPerintahQuery(perintah); //kirim ke command
+            string perintah = $"INSERT INTO pegawais (id, nama, email, username, password, roles) " +
+                $"VALUES ('{pegawai.Id}', '{pegawai.Nama}', '{pegawai.Email}', '{pegawai.Username}', '{pegawai.Password}'" +
+                $", '{pegawai.Role}');";
+
+            Koneksi.JalankanPerintahQuery(perintah);
         }
 
-        public static void HapusData(int idHapus)
+        public static void HapusData(string idHapus)
         {
-            //susun perintah query
-            string perintah = "delete from pegawais where id='" + idHapus + "';";
-            Koneksi.JalankanPerintahQuery(perintah); //kirim ke command
+            string perintah = $"DELETE FROM pegawais WHERE id='{idHapus}';";
+
+            Koneksi.JalankanPerintahQuery(perintah);
         }
 
-        public static List<Pegawai> BacaData(string filter = "", string nilai = "")
+        public static List<Pegawai> BacaData(string id = "", string nama = "")
         {
-            //susun perintah query
             string perintah;
-            if (filter == "")
+
+            if (id == "")
             {
-                perintah = "select * from pegawais";
+                perintah = $"SELECT * FROM pegawais";
             }
+
             else
             {
-                perintah = "select * from pegawais where " + filter + " like '%" + nilai + "%'";
+                perintah = $"SELECT * FROM pegawais WHERE '{id}' like '%{nama}%'";
             }
-            //eksekusi perintah di atas
-            MySqlDataReader hasil = Koneksi.JalankanPerintahSelect(perintah);
-            List<Pegawai> ListData = new List<Pegawai>();
 
-            //selama masih ada data yang dapat dibaca dari datareader
+            MySqlDataReader hasil = Koneksi.JalankanPerintahSelect(perintah);
+
+            List<Pegawai> listPegawai = new List<Pegawai>();
+
             while (hasil.Read() == true)
-            {   //ambil isi datareader
-                string tampungKode = hasil.GetValue(0).ToString(); //kolom pertama
-                string tampungNama = hasil.GetValue(1).ToString(); //kolom kedua
-                //tampung ke sebuah objek kategori
+            {   
                 Pegawai tampung = new Pegawai();
-                //tambahkan ke list
-                ListData.Add(tampung);
+                tampung.Id = hasil.GetInt32(0);
+                tampung.Nama = hasil.GetValue(1).ToString();
+                tampung.Email = hasil.GetValue(2).ToString();
+                tampung.Username = hasil.GetValue(3).ToString();
+                tampung.Role = hasil.GetValue(4).ToString();
+
+                listPegawai.Add(tampung);
             }
-            //kirim list ke pemanggilnya
-            return ListData;
+
+            return listPegawai;
         }
-        public static Pegawai CekLogin(string id, string pwd)
+        public static Pegawai CekLogin(string username, string pswd)
         {
-            string perintah = "SELECT * from pegawais p where p.id='"
-                    + id + "' and p.password = '" + pwd + "'";
+            string perintah = $"SELECT * FROM pegawais p WHERE p.username='{username}' and p.password = '{pswd}'";
             //eksekusi perintah di atas
             MySqlDataReader hasil = Koneksi.JalankanPerintahSelect(perintah);
             //selama masih ada data yang dapat dibaca dari datareader
@@ -101,8 +104,10 @@ namespace Celikoor_LIB
                 tampung.Email = hasil.GetValue(2).ToString();
                 tampung.Username = hasil.GetValue(3).ToString();
                 tampung.Role = hasil.GetValue(5).ToString();
+
                 return tampung;
             }
+
             else return null;
         }
     }
