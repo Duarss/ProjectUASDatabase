6d5964d4259caa2ced5a006ce1f183986d66a507
WILLY HIMAWAN <s160422011@microsoft.ubaya.ac.id>
2023-12-21 23:41:52 +0700

    w

--- Celikoor_Tixycket/Properties/Resources.Designer.cs
+++ Celikoor_Tixycket/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace Celikoor_Tixycket.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap opening4 {
+            get {
+                object obj = ResourceManager.GetObject("opening4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap poster1 {
             get {
                 object obj = ResourceManager.GetObject("poster1", resourceCulture);
