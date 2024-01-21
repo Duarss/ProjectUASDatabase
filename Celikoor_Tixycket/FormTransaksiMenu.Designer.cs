34932800072fbb02868f1742a89c195fe7ecab2a
Duarss <s160422035@student.ubaya.ac.id>
2024-01-21 18:38:02 +0700

    edit text formTransaksiMenu dan logo Tixie

--- Celikoor_Tixycket/FormTransaksiMenu.Designer.cs
+++ Celikoor_Tixycket/FormTransaksiMenu.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTransaksiMenu));
             this.dataGridViewMenu = new System.Windows.Forms.DataGridView();
             this.menu = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -69,12 +70,13 @@
             this.menu,
             this.quantity,
             this.totalPrice});
-            this.dataGridViewMenu.Location = new System.Drawing.Point(12, 280);
+            this.dataGridViewMenu.Location = new System.Drawing.Point(14, 350);
+            this.dataGridViewMenu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridViewMenu.Name = "dataGridViewMenu";
             this.dataGridViewMenu.ReadOnly = true;
             this.dataGridViewMenu.RowHeadersWidth = 51;
             this.dataGridViewMenu.RowTemplate.Height = 24;
-            this.dataGridViewMenu.Size = new System.Drawing.Size(773, 215);
+            this.dataGridViewMenu.Size = new System.Drawing.Size(870, 269);
             this.dataGridViewMenu.TabIndex = 48;
             this.dataGridViewMenu.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewMenu_CellContentClick);
             // 
@@ -107,9 +109,9 @@
             this.label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label.ForeColor = System.Drawing.Color.Black;
-            this.label.Location = new System.Drawing.Point(292, 21);
+            this.label.Location = new System.Drawing.Point(328, 26);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(186, 28);
+            this.label.Size = new System.Drawing.Size(232, 34);
             this.label.TabIndex = 55;
             this.label.Text = "Ordered Item";
             this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -130,24 +132,27 @@
             this.panel2.Controls.Add(this.comboBoxFood);
             this.panel2.Controls.Add(this.label);
             this.panel2.Controls.Add(this.labelDurasi);
-            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Location = new System.Drawing.Point(14, 15);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(773, 218);
+            this.panel2.Size = new System.Drawing.Size(870, 272);
             this.panel2.TabIndex = 56;
             // 
             // numericUpDownQtyBeverage
             // 
-            this.numericUpDownQtyBeverage.Location = new System.Drawing.Point(495, 125);
+            this.numericUpDownQtyBeverage.Location = new System.Drawing.Point(557, 156);
+            this.numericUpDownQtyBeverage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.numericUpDownQtyBeverage.Name = "numericUpDownQtyBeverage";
-            this.numericUpDownQtyBeverage.Size = new System.Drawing.Size(94, 22);
+            this.numericUpDownQtyBeverage.Size = new System.Drawing.Size(106, 26);
             this.numericUpDownQtyBeverage.TabIndex = 141;
             this.numericUpDownQtyBeverage.ValueChanged += new System.EventHandler(this.numericUpDownQtyBeverage_ValueChanged);
             // 
             // numericUpDownQtyFood
             // 
-            this.numericUpDownQtyFood.Location = new System.Drawing.Point(122, 125);
+            this.numericUpDownQtyFood.Location = new System.Drawing.Point(137, 156);
+            this.numericUpDownQtyFood.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.numericUpDownQtyFood.Name = "numericUpDownQtyFood";
-            this.numericUpDownQtyFood.Size = new System.Drawing.Size(94, 22);
+            this.numericUpDownQtyFood.Size = new System.Drawing.Size(106, 26);
             this.numericUpDownQtyFood.TabIndex = 140;
             this.numericUpDownQtyFood.ValueChanged += new System.EventHandler(this.numericUpDownQtyFood_ValueChanged);
             // 
@@ -158,9 +163,9 @@
             this.labelNominalTotalBeverage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.labelNominalTotalBeverage.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelNominalTotalBeverage.ForeColor = System.Drawing.Color.Black;
-            this.labelNominalTotalBeverage.Location = new System.Drawing.Point(588, 166);
+            this.labelNominalTotalBeverage.Location = new System.Drawing.Point(662, 208);
             this.labelNominalTotalBeverage.Name = "labelNominalTotalBeverage";
-            this.labelNominalTotalBeverage.Size = new System.Drawing.Size(21, 20);
+            this.labelNominalTotalBeverage.Size = new System.Drawing.Size(27, 25);
             this.labelNominalTotalBeverage.TabIndex = 139;
             this.labelNominalTotalBeverage.Text = "0";
             this.labelNominalTotalBeverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -172,9 +177,9 @@
             this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label5.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(388, 166);
+            this.label5.Location = new System.Drawing.Point(436, 208);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(194, 20);
+            this.label5.Size = new System.Drawing.Size(242, 25);
             this.label5.TabIndex = 138;
             this.label5.Text = "Beverage Price: Rp.";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -186,9 +191,9 @@
             this.labelTotalNominalFood.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.labelTotalNominalFood.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTotalNominalFood.ForeColor = System.Drawing.Color.Black;
-            this.labelTotalNominalFood.Location = new System.Drawing.Point(189, 166);
+            this.labelTotalNominalFood.Location = new System.Drawing.Point(213, 208);
             this.labelTotalNominalFood.Name = "labelTotalNominalFood";
-            this.labelTotalNominalFood.Size = new System.Drawing.Size(21, 20);
+            this.labelTotalNominalFood.Size = new System.Drawing.Size(27, 25);
             this.labelTotalNominalFood.TabIndex = 137;
             this.labelTotalNominalFood.Text = "0";
             this.labelTotalNominalFood.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -200,9 +205,9 @@
             this.labelMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.labelMenu.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelMenu.ForeColor = System.Drawing.Color.Black;
-            this.labelMenu.Location = new System.Drawing.Point(30, 166);
+            this.labelMenu.Location = new System.Drawing.Point(34, 208);
             this.labelMenu.Name = "labelMenu";
-            this.labelMenu.Size = new System.Drawing.Size(153, 20);
+            this.labelMenu.Size = new System.Drawing.Size(188, 25);
             this.labelMenu.TabIndex = 136;
             this.labelMenu.Text = "Food Price: Rp.";
             this.labelMenu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -214,9 +219,9 @@
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(389, 125);
+            this.label3.Location = new System.Drawing.Point(438, 156);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 18);
+            this.label3.Size = new System.Drawing.Size(104, 22);
             this.label3.TabIndex = 133;
             this.label3.Text = "Quantity:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -228,9 +233,9 @@
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(31, 125);
+            this.label2.Location = new System.Drawing.Point(35, 156);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.Size = new System.Drawing.Size(104, 22);
             this.label2.TabIndex = 131;
             this.label2.Text = "Quantity:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -240,9 +245,10 @@
             this.comboBoxBeverage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxBeverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxBeverage.FormattingEnabled = true;
-            this.comboBoxBeverage.Location = new System.Drawing.Point(495, 75);
+            this.comboBoxBeverage.Location = new System.Drawing.Point(557, 94);
+            this.comboBoxBeverage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBoxBeverage.Name = "comboBoxBeverage";
-            this.comboBoxBeverage.Size = new System.Drawing.Size(239, 26);
+            this.comboBoxBeverage.Size = new System.Drawing.Size(268, 30);
             this.comboBoxBeverage.TabIndex = 61;
             this.comboBoxBeverage.SelectedIndexChanged += new System.EventHandler(this.comboBoxBeverage_SelectedIndexChanged);
             // 
@@ -253,9 +259,9 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(388, 78);
+            this.label1.Location = new System.Drawing.Point(436, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 18);
+            this.label1.Size = new System.Drawing.Size(124, 22);
             this.label1.TabIndex = 60;
             this.label1.Text = "Beverages:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -265,9 +271,10 @@
             this.comboBoxFood.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxFood.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxFood.FormattingEnabled = true;
-            this.comboBoxFood.Location = new System.Drawing.Point(122, 75);
+            this.comboBoxFood.Location = new System.Drawing.Point(137, 94);
+            this.comboBoxFood.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.comboBoxFood.Name = "comboBoxFood";
-            this.comboBoxFood.Size = new System.Drawing.Size(239, 26);
+            this.comboBoxFood.Size = new System.Drawing.Size(268, 30);
             this.comboBoxFood.TabIndex = 59;
             this.comboBoxFood.SelectedIndexChanged += new System.EventHandler(this.comboBoxFood_SelectedIndexChanged);
             // 
@@ -278,9 +285,9 @@
             this.labelDurasi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.labelDurasi.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelDurasi.ForeColor = System.Drawing.Color.Black;
-            this.labelDurasi.Location = new System.Drawing.Point(58, 78);
+            this.labelDurasi.Location = new System.Drawing.Point(65, 98);
             this.labelDurasi.Name = "labelDurasi";
-            this.labelDurasi.Size = new System.Drawing.Size(54, 18);
+            this.labelDurasi.Size = new System.Drawing.Size(66, 22);
             this.labelDurasi.TabIndex = 54;
             this.labelDurasi.Text = "Food:";
             this.labelDurasi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -291,10 +298,10 @@
             this.buttonTambahGenre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonTambahGenre.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonTambahGenre.ForeColor = System.Drawing.Color.White;
-            this.buttonTambahGenre.Location = new System.Drawing.Point(13, 237);
-            this.buttonTambahGenre.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonTambahGenre.Location = new System.Drawing.Point(15, 296);
+            this.buttonTambahGenre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonTambahGenre.Name = "buttonTambahGenre";
-            this.buttonTambahGenre.Size = new System.Drawing.Size(215, 36);
+            this.buttonTambahGenre.Size = new System.Drawing.Size(242, 45);
             this.buttonTambahGenre.TabIndex = 130;
             this.buttonTambahGenre.Text = "ADD";
             this.buttonTambahGenre.UseVisualStyleBackColor = false;
@@ -306,10 +313,10 @@
             this.buttonKeluar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonKeluar.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonKeluar.ForeColor = System.Drawing.Color.White;
-            this.buttonKeluar.Location = new System.Drawing.Point(12, 501);
+            this.buttonKeluar.Location = new System.Drawing.Point(14, 626);
             this.buttonKeluar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonKeluar.Name = "buttonKeluar";
-            this.buttonKeluar.Size = new System.Drawing.Size(143, 39);
+            this.buttonKeluar.Size = new System.Drawing.Size(161, 49);
             this.buttonKeluar.TabIndex = 58;
             this.buttonKeluar.Text = "&CLOSE";
             this.buttonKeluar.UseVisualStyleBackColor = false;
@@ -321,10 +328,10 @@
             this.buttonSimpan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSimpan.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSimpan.ForeColor = System.Drawing.Color.White;
-            this.buttonSimpan.Location = new System.Drawing.Point(637, 501);
+            this.buttonSimpan.Location = new System.Drawing.Point(717, 626);
             this.buttonSimpan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonSimpan.Name = "buttonSimpan";
-            this.buttonSimpan.Size = new System.Drawing.Size(148, 39);
+            this.buttonSimpan.Size = new System.Drawing.Size(166, 49);
             this.buttonSimpan.TabIndex = 57;
             this.buttonSimpan.Text = "&SAVE";
             this.buttonSimpan.UseVisualStyleBackColor = false;
@@ -337,27 +344,29 @@
             this.labelSisaKursi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.labelSisaKursi.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelSisaKursi.ForeColor = System.Drawing.Color.Red;
-            this.labelSisaKursi.Location = new System.Drawing.Point(382, 237);
+            this.labelSisaKursi.Location = new System.Drawing.Point(430, 296);
             this.labelSisaKursi.Name = "labelSisaKursi";
-            this.labelSisaKursi.Size = new System.Drawing.Size(403, 18);
+            this.labelSisaKursi.Size = new System.Drawing.Size(497, 22);
             this.labelSisaKursi.TabIndex = 131;
             this.labelSisaKursi.Text = "(Make the quantity 0 if you don\'t want to order)";
             this.labelSisaKursi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormTransaksiMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(174)))), ((int)(((byte)(66)))));
-            this.ClientSize = new System.Drawing.Size(797, 551);
+            this.ClientSize = new System.Drawing.Size(897, 689);
             this.Controls.Add(this.labelSisaKursi);
             this.Controls.Add(this.buttonKeluar);
             this.Controls.Add(this.buttonSimpan);
             this.Controls.Add(this.dataGridViewMenu);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.buttonTambahGenre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormTransaksiMenu";
-            this.Text = "FormTransaksiMenu";
+            this.Text = "TIXIE";
             this.Load += new System.EventHandler(this.FormTransaksiMenu_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMenu)).EndInit();
             this.panel2.ResumeLayout(false);
